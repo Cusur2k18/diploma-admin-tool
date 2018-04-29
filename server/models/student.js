b9507a2ddc0dfcb25b9402585c54018a678251f9
Cristofer Flores <cristofer.flort@gmail.com>
2018-04-29 11:36:24 -0500

    Created the remote method to sign in users but with the whole udg integration on it

--- server/models/student.js
+++ server/models/student.js
@@ -4,6 +4,28 @@ const StudentService = require('../shared/services/student-service');
 
 module.exports = function(Student) {
 
-  Student.beforeRemote('login', StudentService.validateCredentials);
+  /**
+   * @description Remote method to sign in users
+   * @param {*} studentCode 
+   * @param {*} nip 
+   * @param {*} cb 
+   */
+  Student.signin = function(studentCode, nip, cb) {
+
+    StudentService.signin(studentCode, nip, cb)
+  }
+
+  Student.remoteMethod('signin', {
+    accepts: [
+      {arg: 'studentCode', type: 'string'},
+      {arg: 'nip', type: 'string'}
+    ],
+    returns: {arg: 'response', type: 'object'},
+    description: 'Sign in users, use this instead of \'/Login\' ',
+    http: {
+      status: 200,
+      errorStatus: 412
+    }
+  });
 
 };
--- server/shared/services/student-service.js
+++ server/shared/services/student-service.js
@@ -2,25 +2,82 @@
 
 const app = require('../../server');
 const BaseApiService = require('./base-api-service');
+const async = require('async');
 
 class StudentService extends BaseApiService {
+
+  /**
+   * 
+   * @description Model instance of Student
+   * @readonly
+   * @static
+   * @memberof StudentService
+   */
   static get model() {
+
     return app.models.Student;
   }
+  /**
+   * 
+   * @description This method sign in the users by first checking in the udg database
+   *              then try to find it or create a student
+   * @static
+   * @param {any} studentCode 
+   * @param {any} nip 
+   * @param {any} cb 
+   * @memberof StudentService
+   */
+  static signin(studentCode, nip, cb) {
+    const udgalumnoModel = app.models.DatosAlumno;
 
-  static validateCredentials(ctx, _, next) {
-    let studentCode = ctx.req.body.username;
-    let nip = ctx.req.body.password;
-    const udgalumno = app.models.DatosAlumno
-    udgalumno.datosAlumno({pCodigo: studentCode, pNip: nip }, function (err, response) {
-      if (err) return next(new Error(err));
+    
+    /** 
+     * 1.- VALIDATE DATA WITH UDG DATABASE
+     * 2.- FIND OR CREATE THE STUDENT IN OUR DB BASED ON THE STUDENT CODE
+     * 3.- LOGIN THE USER WE CREATED OR FOUND
+    */
+    async.waterfall([
+      (callback) => { 
+        udgalumnoModel.datosAlumno({pCodigo: studentCode, pNip: nip }, (err, response) => {
 
-      var result = response;
-      // cb(err, result);
-      console.log(result);
-      ctx.res.studentData = response;
-      return next(err, response);
-    });
+          callback(err, Object.assign({}, JSON.parse(response.return), {nip: nip}));
+        });
+      },
+      (data, callback) => {
+        console.log('data: ', data);
+
+        if (data.error) return callback(new Error(data.error));
+
+        let names = data.nombre.split(' '), 
+            studentModel = {
+              studentCode: data.codigo,
+              career: data.carrera_desc,
+              careerGrade: data.estatus_desc,
+              firstName: names[2],
+              lastName: names[0],
+              mLastName: names[1],
+              username: data.codigo,
+              password: data.nip
+            }
+        console.log('studentModel: ', studentModel);
+
+        this.model.findOrCreate(
+          { where: {studentCode: data.codigo} }, 
+          studentModel, 
+          (err, instance, created) => {
+            const student = instance.toJSON();
+
+            if (err) return callback(new Error(err));
+            callback(null, Object.assign({}, student, {nip: data.nip}))
+          });
+      },
+      (student, callback) => {
+        this.model.login({username: student.studentCode, password: student.nip}, 'user', (err, token) => {
+          if (err) return callback(new Error(err));
+          callback(null, token);
+        })
+      }
+    ], cb);
   }
 
 }
