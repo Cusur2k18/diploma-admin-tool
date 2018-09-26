008b30601fb2dbf4abd0c08398f9dd41eddbc4d9
Christofer Flores <christofer@michelada.io>
2018-09-26 17:56:04 -0500

    Modified the uuid mixin to fix some issues

--- server/mixins/uuid.js
+++ server/mixins/uuid.js
@@ -1,5 +1,16 @@
+const uuid = require('uuid/v1');
+
 module.exports = function(Model, options) {
   // Model is the model class
   // options is an object containing the config properties from model definition
-  Model.defineProperty('uuid', {type: String, defaultFn: "$uuid"});
+  Model.defineProperty('uuid', {type: String, defaultFn: "$uuid"})
+  
+  Model.observe('before save', function event(ctx, next) {
+    if (ctx.instance) {
+      ctx.instance.uuid = uuid();
+    } else {
+      ctx.data.uuid = uuid();
+    }
+    next();
+  });
 }
