94db0ab6643e07af6600bb0c09c887f87de58602
Christofer Flores <christofer@michelada.io>
2018-09-27 14:38:16 -0500

    Add the information for production

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -5,5 +5,15 @@ module.exports = {
     "wsdl": process.env.UDG_SOAP_SERVICE_URL,
     "remotingEnabled": true,
     "connector": "soap"
+  },
+  cusurdb: {
+    "host": process.env.DB_HOST,
+    "port": 5432,
+    "url": process.env.DATABASE_URL,
+    "database": process.env.DB_NAME,
+    "password": process.env.DB_PASS,
+    "name": "cusurdb",
+    "user": process.env.DB_USER,
+    "connector": "postgresql"
   }
 }
